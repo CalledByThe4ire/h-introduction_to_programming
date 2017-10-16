ff4c7c659925b19c143329f42ab9b1fdeaa9b886
Andrey Karamnoff <karamnoff@gmail.com>
2017-10-16 23:39:32 +0300

    add description

--- src/half.js
+++ src/half.js
@@ -1,5 +1,26 @@
-// @flow
+const categories = [
+  { id: 'animals', parent: null },
+  { id: 'mammals', parent: 'animals' },
+  { id: 'cats', parent: 'mammals' },
+  { id: 'dogs', parent: 'mammals' },
+  { id: 'chihuahua', parent: 'dogs' },
+  { id: 'labrador', parent: 'dogs' },
+  { id: 'persian', parent: 'cats' },
+  { id: 'siamese', parent: 'cats' },
+];
 
-export default function (num: number) {
-  return num / 2;
-}
+const makeTree = (categories, parent) => {
+  const node = {};
+  categories
+    .filter(category => category.parent === parent)
+    .forEach((category) => {
+      node[category.id] = makeTree(categories, category.id);
+    });
+  return node;
+};
+
+console.log(
+  JSON.stringify(
+    makeTree(categories, null)
+    , null, 2)
+);
