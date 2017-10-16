f30b77eb90aaabe042997bb1a034df5723331c3c
Andrey Karamnoff <karamnoff@gmail.com>
2017-10-17 00:05:52 +0300

    add solution

--- src/sequenceSum.js
+++ src/sequenceSum.js
@@ -1,7 +1,20 @@
 const sequenceSum = (begin, end) => {
   // BEGIN (write your solution here)
-
+  if (begin > end) {
+    return NaN;
+  }
+  if (begin === end) {
+    return end;
+  }
+  // 2 + sequenceSum(2 + 1, 6) // 2 + 18
+  // 3 + sequenceSum(3 + 1, 6) // 3 + 15
+  // 4 + sequenceSum(4 + 1, 6) // 4 + 11
+  // 5 + sequenceSum(5 + 1, 6) // 5 + 6
+  return begin + sequenceSum(begin + 1, end);
   // END
 };
+
+// Visualize Execution: https://goo.gl/UlTxCs
+
 
 export default sequenceSum;
