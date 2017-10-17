8049397fd8d6c6132015285d112917547b086f96
Andrey Karamnoff <karamnoff@gmail.com>
2017-10-17 23:25:51 +0300

    add solution

--- src/smallestDivisor.js
+++ src/smallestDivisor.js
@@ -1,7 +1,33 @@
 const smallestDivisor = (num) => {
-  // BEGIN (write your solution here)
-
-  // END
+  if (num <= 0) {
+    return 'Введите положительное число';
+  }
+  if (num === 1) {
+    return 1;
+  }
+  const iter = (acc) => {
+    if (num % acc !== 0) {
+      return iter(acc + 1);
+    }
+    return acc;
+  };
+  return iter(2);
 };
 
 export default smallestDivisor;
+
+// tutor's solution
+/* const iter = (acc) => {
+  // We use 'num / 2' in the condition below, and not 'num'.
+  // This is a simple optimization: a number cannot be divided
+  // by a number larger than its half.
+  if (acc > num / 2) {
+    return num;
+  }
+  if (num % acc === 0) {
+    return acc;
+  }
+  return iter(acc + 1);
+};
+
+return iter(2); */
