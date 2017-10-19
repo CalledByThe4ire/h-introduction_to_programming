19c4851d5c0d83d492b7449a55b420172b0a9c00
Andrey Karamnoff <karamnoff@gmail.com>
2017-10-20 00:23:25 +0300

    add solution

--- src/addDigits.js
+++ src/addDigits.js
@@ -1,5 +1,23 @@
 import { length } from './strings'; // eslint-disable-line
 
-// BEGIN (write your solution here)
+// BEGIN
+const sum = (str) => {
+  let result = 0;
+  for (let i = 0; i < length(str); i += 1) {
+    result += Number(str[i]);
+  }
 
+  return result;
+};
+
+const addDigits = (num) => {
+  let result = num;
+  while (result >= 10) {
+    result = sum(String(result));
+  }
+
+  return result;
+};
+
+export default addDigits;
 // END
